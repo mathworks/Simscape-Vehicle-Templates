d5867c4d9d737782177cf9b48e560f63b7d74e4b
Steve Miller <smiller@mathworks.com>
2020-10-30 11:36:36 +0100

    Update default trajectories (CRG MP, CRG NN). Stop simulation at end of lap (CRG events).

--- Libraries/Event/Scene/Double_Lane_Change/sm_car_trajectory_double_lane_change.m
+++ Libraries/Event/Scene/Double_Lane_Change/sm_car_trajectory_double_lane_change.m
@@ -2,37 +2,42 @@ function sm_car_trajectory_double_lane_change
 % Function to construct double-lane change maneuver
 % Copyright 2018-2020 The MathWorks, Inc.
 
-cd(which(mfilename))
+cd(fileparts(which(mfilename)))
 % Parameters for trajectory
 maneuver_lateral_offset    = -3.35; % m
 cone_set_lat_separation    = 4.2;   % m
 
 % Longitudinal gate locations (m)
 gate0       = 0;     % Start 
+
 gate1       = 175;   % Entry 1st set of cones
 gate2       = 187;   % Exit  1st set of cones
+
 gate3       = 198;   % Entry 2nd set of cones
 gate4       = 213;   % Exit  2nd set of cones
+
 gate5       = 224;   % Entry 3rd set of cones
 gate6       = 240;   % Entry 3rd set of cones
+
 gate7       = 290;   % End
 
 v_lc        = 12;     % Speed during lane change (m/s)
 x_testv     = 50;    % Distance when target speed is reached (m)
 
-% Assemble waypoints
-x_waypoints =  [gate0 x_testv gate1:1:gate2 gate3:1:gate4 gate5:1:gate6];
-y_waypoints =  [0 0 zeros(1,gate2-gate1+1) ones(1,gate4-gate3+1) zeros(1,gate6-gate5+1)]*cone_set_lat_separation+maneuver_lateral_offset;
-vx_waypoints = [1 v_lc v_lc*ones(1,gate2-gate1+1) v_lc*ones(1,gate4-gate3+1) v_lc*ones(1,gate6-gate5+1)];
+% Assemble waypoints, foundation for interpolation
+x_waypoints =  [gate0 x_testv   gate1:1:gate2              gate3:1:gate4              gate5:1:gate6             ];
+y_waypoints =  [0     0         zeros(1,gate2-gate1+1)     ones(1,gate4-gate3+1)      zeros(1,gate6-gate5+1)    ]*cone_set_lat_separation+maneuver_lateral_offset;
+vx_waypoints = [1     v_lc      v_lc*ones(1,gate2-gate1+1) v_lc*ones(1,gate4-gate3+1) v_lc*ones(1,gate6-gate5+1)];
 
-% Construct sample points
-x_new  = [gate0:(gate1-gate0)/30:(gate1-(gate1-gate0)/30) ...
-    gate1:(gate2-gate1)/10:(gate2-(gate2-gate1)/10) ...
-    gate2:(gate3-gate2)/20:  (gate3-(gate3-gate2)/20) ...
-    gate3:(gate4-gate3)/10:  (gate4-(gate4-gate3)/10) ...
-    gate4:(gate5-gate4)/20:  (gate5-(gate5-gate4)/20) ...
-    gate5:(gate6-gate5)/15:  (gate6-(gate6-gate5)/15) ...
-    gate6:(gate7-gate6)/10:  (gate7)];
+% Construct query points for interpolation
+x_new  = [...
+    gate0:(gate1-gate0)/30: (gate1-(gate1-gate0)/30) ...
+    gate1:(gate2-gate1)/10: (gate2-(gate2-gate1)/10) ...
+    gate2:(gate3-gate2)/20: (gate3-(gate3-gate2)/20) ...
+    gate3:(gate4-gate3)/10: (gate4-(gate4-gate3)/10) ...
+    gate4:(gate5-gate4)/20: (gate5-(gate5-gate4)/20) ...
+    gate5:(gate6-gate5)/15: (gate6-(gate6-gate5)/15) ...
+    gate6:(gate7-gate6)/10: (gate7)];
 
 % Interpolate to get lateral distance and speed
 y_new  = interp1(x_waypoints, y_waypoints,  x_new, 'pchip');
@@ -107,6 +112,10 @@ y.Value = y_new;
 y.Units = 'm';
 y.Comments = '';
 
+z.Value = zeros(size(x_new));
+z.Units = 'm';
+z.Comments = '';
+
 xTrajectory.Value = xTrajectory_new;
 xTrajectory.Units = 'm';
 xTrajectory.Comments = 'Distance traveled';
@@ -119,5 +128,5 @@ aYaw.Value = aYaw_new;
 aYaw.Units = 'rad';
 aYaw.Comments = 'Yaw Angle, non-wrapping';
 
-save Double_Lane_Change_trajectory_default x y vx aYaw xTrajectory 
+save Double_Lane_Change_trajectory_default x y z vx aYaw xTrajectory 
 
