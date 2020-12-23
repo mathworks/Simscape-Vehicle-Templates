adb61e4aa74f56ccc3bb2265c9e39b314c185355
Steve Miller <smiller@mathworks.com>
2020-12-23 13:14:57 +0100

    Added lateral-only slosh to tanker trailer. Updated Mask descriptions.

--- Scripts_Data/Data_Vehicle/Presets/sm_car_assemble_presets_vehicle_axle3.m
+++ Scripts_Data/Data_Vehicle/Presets/sm_car_assemble_presets_vehicle_axle3.m
@@ -8,7 +8,7 @@ cd(fileparts(which(mfilename)));
 veh_3Axle_ind = 0;
 vehNamePref   = 'Vehicle_Axle3_';
 
-%%  MFeval 15DOF 6x2
+%% Bus_Makhulu_3Axle 15DOF MFeval 6x2 
 vehcfg_set = {
     'Aero',         'Bus_Makhulu',        '';...
     'Body',         'Bus_Makhulu_3Axle',      '';...
@@ -48,7 +48,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFeval 15DOF 6x4 
+%%  Bus Makhulu 3Axle 15DOF MFeval 6x4 
 drv_ind = strcmp(vehcfg_set(:,1),'Driveline');
 vehcfg_set{drv_ind,2} = 'A11D_A21D3_A31D3_BM3';
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
@@ -60,7 +60,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%%  MFSwift 15DOF 6x2
+%%  Bus Makhulu 3Axle 15DOF MFSwift 6x2
 vehcfg_set = {
     'Aero',         'Bus_Makhulu',        '';...
     'Body',         'Bus_Makhulu_3Axle',      '';...
@@ -100,7 +100,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFSwift 15DOF 6x4 config
+%% Bus Makhulu 3Axle MFSwift 15DOF 6x4
 drv_ind = strcmp(vehcfg_set(:,1),'Driveline');
 vehcfg_set{drv_ind,2} = 'A11D_A21D3_A31D3_BM3';
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
@@ -113,7 +113,7 @@ save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
 
-%% MFEval dwb 6x2
+%% Bus Makhulu 3Axle MFEval dwb 6x2
 vehcfg_set = {
     'Aero',         'Bus_Makhulu',        '';...
     'Body',         'Bus_Makhulu_3Axle',      '';...
@@ -153,7 +153,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% 6x4 config
+%% Bus Makhulu 3Axle MFEval dwb 6x4 
 drv_ind = strcmp(vehcfg_set(:,1),'Driveline');
 vehcfg_set{drv_ind,2} = 'A11D_A21D3_A31D3_BM3';
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
@@ -165,7 +165,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFSwift dwb 6x2
+%% Bus Makhulu 3Axle MFSwift dwb 6x2
 vehcfg_set = {
     'Aero',         'Bus_Makhulu',        '';...
     'Body',         'Bus_Makhulu_3Axle',      '';...
@@ -205,7 +205,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFSwift dwb 6x4
+%% Bus Makhulu 3Axle MFSwift dwb 6x4
 drv_ind = strcmp(vehcfg_set(:,1),'Driveline');
 vehcfg_set{drv_ind,2} = 'A11D_A21D3_A31D3_BM3';
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
@@ -217,7 +217,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%%  MFeval Amandla 15DOF 6x2
+%%  Truck Amandla MFeval 15DOF 6x2
 vehcfg_set = {
     'Aero',         'Truck_Amandla',                     '';...
     'Body',         'Truck_Amandla_3Axle',               '';...
@@ -258,7 +258,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFeval, Amandla, 6x4 config
+%% Truck Amandla MFeval 6x4
 drv_ind = strcmp(vehcfg_set(:,1),'Driveline');
 vehcfg_set{drv_ind,2} = 'A11D_A21D3_A31D3_TA3';
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
@@ -270,7 +270,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFSwift, Amandla, 6x2
+%% Truck Amandla MFSwift 6x2
 vehcfg_set = {
     'Aero',         'Truck_Amandla',                     '';...
     'Body',         'Truck_Amandla_3Axle',               '';...
@@ -311,7 +311,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFSwift, Amandla, 6x4 config
+%% Truck Amandla MFSwift 6x4
 drv_ind = strcmp(vehcfg_set(:,1),'Driveline');
 vehcfg_set{drv_ind,2} = 'A11D_A21D3_A31D3_TA3';
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
@@ -323,7 +323,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%%  MFeval Semi Truck 15DOF 6x2
+%%  Semi Truck 15DOF MFeval 6x2
 vehcfg_set = {
     'Aero',         'Truck_Amandla',                     '';...
     'Body',         'Truck_Amandla_3Axle',               '';...
@@ -350,12 +350,12 @@ vehcfg_set = {
     'TireDyn',      'steady',                            'TireA2';
     'TireDyn',      'steady',                            'TireA3';
     'Driveline',    'A11D_A21D3_A31D_TA3',               ''};
-
+                      
 assignin('base','vehcfg_set',vehcfg_set); % For debugging
 
 % Assemble vehicle
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
-Vehicle.config = 'Amandla3Axle_15DOF_MFEval_steady_6x2Generic';
+Vehicle.config = 'Amandla3Axle_15DOF_MFEval_steady_6x2Gen';
 %Init = IDatabase.Flat.Bus_Makhulu_Axle3;
 
 veh_3Axle_ind = veh_3Axle_ind+1;
@@ -364,7 +364,7 @@ eval([veh_var_name ' = Vehicle;']);
 save(veh_var_name,veh_var_name);
 disp([pad(veh_var_name,12) ': ' Vehicle.config]);
 
-%% MFSwift Semi Truck 15DOF 6x2
+%% Semi Truck 15DOF MFSwift 6x2
 tire_ind = find(strcmp(vehcfg_set(:,1),'Tire'));
 tirA1_ind = find(strcmp(vehcfg_set(:,3),'TireA1'));
 vehcfg_set{intersect(tire_ind,tirA1_ind),2} = 'MFEval_Generic_430_50R38';
@@ -374,7 +374,66 @@ tirA3_ind = find(strcmp(vehcfg_set(:,3),'TireA3'));
 vehcfg_set{intersect(tire_ind,tirA3_ind),2} = 'MFEval_2x_Generic_430_50R38';
 assignin('base','vehcfg_set',vehcfg_set);
 Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
-Vehicle.config = 'Amandla3Axle_15DOF_MFSwift_steady_6x2Generic';
+Vehicle.config = 'Amandla3Axle_15DOF_MFSwift_steady_6x2Gen';
+
+veh_3Axle_ind = veh_3Axle_ind+1;
+veh_var_name = [vehNamePref pad(num2str(veh_3Axle_ind),3,'left','0')]; 
+eval([veh_var_name ' = Vehicle;']);
+save(veh_var_name,veh_var_name);
+disp([pad(veh_var_name,12) ': ' Vehicle.config]);
+
+%%  Semi Truck 15DOF MFSwift 6x2
+vehcfg_set = {
+    'Aero',         'Truck_Amandla',                     '';...
+    'Body',         'Truck_Amandla_3Axle',               '';...
+    'BodyGeometry', 'Semi_Truck_Amandla',               '';...
+    'Passenger',    'None',                              '';...
+    'Power',        'Ideal_A2_default',                  '';...
+    'Brakes',       'Axle3_PedalAbstract_DiscDiscDisc_Truck_Amandla_Axle3',    '';...
+    'Springs',      'Axle3_None',                        'None_None_None';...
+    'Dampers',      'Axle3_None',                        'None_None_None';...
+    'Susp',         'Simple15DOF_Truck_Amandla_A1',      'SuspA1';
+    'Susp',         'Simple15DOF_Truck_Amandla_A2',      'SuspA2';
+    'Susp',         'Simple15DOF_Truck_Amandla_A3',      'SuspA3';
+    'Steer',        'Ackermann_Amandla_A1',              'SuspA1';...
+    'Steer',        'None_default',                      'SuspA2';...
+    'Steer',        'None_default',                      'SuspA3';...
+    'DriverHuman',  'Truck_Amandla',                     'SuspA1';...
+    'AntiRollBar',  'None',                              'SuspA1';...
+    'AntiRollBar',  'None',                              'SuspA2';...
+    'AntiRollBar',  'None',                              'SuspA3';...
+    'Tire',         'MFEval_Generic_430_50R38',          'TireA1';
+    'Tire',         'MFEval_2x_Generic_430_50R38',       'TireA2';
+    'Tire',         'MFEval_2x_Generic_430_50R38',       'TireA3';
+    'TireDyn',      'steady',                            'TireA1';
+    'TireDyn',      'steady',                            'TireA2';
+    'TireDyn',      'steady',                            'TireA3';
+    'Driveline',    'A11D_A21D3_A31D3_TA3',               ''};
+
+assignin('base','vehcfg_set',vehcfg_set); % For debugging
+
+% Assemble vehicle
+Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
+Vehicle.config = 'Amandla3Axle_15DOF_MFEval_steady_6x4Gen';
+%Init = IDatabase.Flat.Bus_Makhulu_Axle3;
+
+veh_3Axle_ind = veh_3Axle_ind+1;
+veh_var_name = [vehNamePref pad(num2str(veh_3Axle_ind),3,'left','0')]; 
+eval([veh_var_name ' = Vehicle;']);
+save(veh_var_name,veh_var_name);
+disp([pad(veh_var_name,12) ': ' Vehicle.config]);
+
+%% Semi Truck 15DOF MFSwift 6x4
+tire_ind = find(strcmp(vehcfg_set(:,1),'Tire'));
+tirA1_ind = find(strcmp(vehcfg_set(:,3),'TireA1'));
+vehcfg_set{intersect(tire_ind,tirA1_ind),2} = 'MFSwift_Generic_430_50R38';
+tirA2_ind = find(strcmp(vehcfg_set(:,3),'TireA2'));
+vehcfg_set{intersect(tire_ind,tirA2_ind),2} = 'MFSwift_2x_Generic_430_50R38';
+tirA3_ind = find(strcmp(vehcfg_set(:,3),'TireA3'));
+vehcfg_set{intersect(tire_ind,tirA3_ind),2} = 'MFSwift_2x_Generic_430_50R38';
+assignin('base','vehcfg_set',vehcfg_set);
+Vehicle = sm_car_vehcfg_assemble_vehicle(vehcfg_set);
+Vehicle.config = 'Amandla3Axle_15DOF_MFSwift_steady_6x4Gen';
 
 veh_3Axle_ind = veh_3Axle_ind+1;
 veh_var_name = [vehNamePref pad(num2str(veh_3Axle_ind),3,'left','0')]; 
