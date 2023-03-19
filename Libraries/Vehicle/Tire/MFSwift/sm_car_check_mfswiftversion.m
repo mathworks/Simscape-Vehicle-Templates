e6efa2ffcc33923340e390bd2db73049d488ccc7
Steve Miller <smiller@mathworks.com>
2023-03-20 00:03:58 +0100

    v2p14. Compatible with MF-Swift v2212, updated hardpoints.

--- Libraries/Vehicle/Tire/MFSwift/sm_car_check_mfswiftversion.m
+++ Libraries/Vehicle/Tire/MFSwift/sm_car_check_mfswiftversion.m
@@ -8,6 +8,9 @@ mask_descr = get_param('mfswift_simscape_library/MF-Swift Simscape','MaskDescrip
 bdclose('mfswift_simscape_library');
 
 check_string = 'MF-Swift v';
+NL = regexp(mask_descr, '[\n]');
+
 vers_str_index = strfind(mask_descr,check_string)+length(check_string);
 
-mfswift_ver = mask_descr(vers_str_index:vers_str_index+5);
+%mfswift_ver = mask_descr(vers_str_index:vers_str_index+5);
+mfswift_ver = mask_descr(vers_str_index:NL(end)-1);
