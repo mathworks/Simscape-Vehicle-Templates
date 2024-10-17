b0e08d0230d47c2cd340f4f8e0717acc6e28c12a
Steve Miller <56474268+smiller01985@users.noreply.github.com>
2024-10-17 20:57:39 +0200

    v3p3 Initial commit

--- Testrigs/Quarter_Car/sm_car_testrig_quarter_car_plot1toecamber.m
+++ Testrigs/Quarter_Car/sm_car_testrig_quarter_car_plot1toecamber.m
@@ -1,13 +1,16 @@
+function TSuspMetrics = sm_car_testrig_quarter_car_plot1toecamber(mdl)
+
 % Code to plot simulation results from sm_car_testrig_quarter_car
 %% Plot Description:
 %
-% <enter plot description here if desired>
+% Plot toe and camber curves during bump test.  Calculate standard
+% suspension performance metrics.
 %
 % Copyright 2018-2024 The MathWorks, Inc.
 
 % Generate simulation results if they don't exist
 if ~exist('simlog_sm_car_testrig_quarter_car', 'var')
-    sim('testrig_quarter_car')
+    out = sim(mdl);
 end
 
 % Reuse figure if it exists, else create new figure
@@ -21,14 +24,30 @@ clf(h1_sm_car_testrig_quarter_car)
 temp_colororder = get(gca,'defaultAxesColorOrder');
 
 % Get simulation results
-simlog_pzTire = simlog_sm_car_testrig_quarter_car.Actuator.Cartesian_Joint.Pz.p.series.values('m');
+simlog_t             = out.simlog_sm_car_testrig_quarter_car.Actuator.Cartesian_Joint.Pz.p.series.time;
+simlog_pzTire        = out.simlog_sm_car_testrig_quarter_car.Actuator.Cartesian_Joint.Pz.p.series.values('m');
+simlog_aCamber       = squeeze(out.logsout_sm_car_testrig_quarter_car.get('aCamber').Values.Data);
+simlog_aToe          = squeeze(out.logsout_sm_car_testrig_quarter_car.get('aToe').Values.Data);
+simlog_aCamberX      = squeeze(out.logsout_sm_car_testrig_quarter_car.get('Whl').Values.aCamberX.Data);
+simlog_aToeX         = squeeze(out.logsout_sm_car_testrig_quarter_car.get('Whl').Values.aToeX.Data);
 
-simlog_aCamber = logsout_sm_car_testrig_quarter_car.get('aCamber');
-simlog_aToe = logsout_sm_car_testrig_quarter_car.get('aToe');
+simlog_testPhases    = out.logsout_sm_car_testrig_quarter_car.find('timeTestPhases');
+if(~isempty(simlog_testPhases))
+    % If testrig performs steer tests, get parameters for post-processing
+    timeTestPhases = reshape(simlog_testPhases.Values.Data,2,[])';
+    simlog_zWChp         = out.logsout_sm_car_testrig_quarter_car.get('zWChp').Values.Data;
+    simlog_zOffsetBumpTest  = out.logsout_sm_car_testrig_quarter_car.get('zOffsetBumpTest').Values.Data;
+    simlog_qToeForCaster = out.logsout_sm_car_testrig_quarter_car.get('qToeForCaster').Values.Data;
+    % Get indices of simulation results for vertical test phase
+    indToeCamb = intersect(find(simlog_t>=timeTestPhases(1,1)),find(simlog_t<=timeTestPhases(1,2)));
+else
+    % Else, generate toe and camber plots from simulation results
+    indToeCamb = 1:length(simlog_t);
+end
 
 % Plot results
 simlog_handles(1) = subplot(1, 2, 2);
-plot(simlog_aCamber.Values.Data(:), simlog_pzTire-simlog_pzTire(1), 'LineWidth', 1)
+plot(simlog_aCamber(indToeCamb), simlog_pzTire(indToeCamb)-simlog_pzTire(1), 'LineWidth', 1)
 grid on
 title('Camber Curve')
 xlabel('Camber (deg)')
@@ -39,13 +58,26 @@ catch
 end
 
 simlog_handles(2) = subplot(1, 2, 1);
-plot(simlog_aToe.Values.Data(:), simlog_pzTire-simlog_pzTire(1), 'LineWidth', 1)
+plot(simlog_aToe(indToeCamb), simlog_pzTire(indToeCamb)-simlog_pzTire(1), 'LineWidth', 1)
 grid on
 title('Toe Curve')
 xlabel('Toe (deg)')
 ylabel('Suspension Travel (m)')
 
-% Remove temporary variables
-clear simlog_handles temp_colororder
-clear simlog_aToe simlog_aCamber simlog_pzTire 
-
+% Calculate suspension metrics
+if(~isempty(simlog_testPhases))
+    TSuspMetrics = sm_car_calc_susp_metrics(...
+        simlog_aToe, ...
+        simlog_aCamber, ...
+        simlog_aToeX,...
+        simlog_aCamberX,...
+        simlog_pzTire, ...
+        timeTestPhases, ...
+        simlog_qToeForCaster,...
+        simlog_zOffsetBumpTest,...
+        simlog_t,...
+        simlog_zWChp...
+        );
+else
+    TSuspMetrics = [];
+end
