e4dd71a27205863212f70c8026fce9c01af81087
Steve Miller <smiller@mathworks.com>
2026-02-24 23:36:56 +0100

    Initial commit v4p10

--- Scripts_Data/Simscape_Vehicle_Library_Demo_Script.m
+++ Scripts_Data/Simscape_Vehicle_Library_Demo_Script.m
@@ -32,10 +32,12 @@
 % <tr>5.  Anti-Lock Brakes: <a href="matlab:sm_car_test_abs">Run</a>, <a href="matlab:edit('sm_car_test_abs.m');">Edit Code</a>, <a href="matlab:web('sm_car_test_abs.png');">Results</a><br>
 % <tr>6.  Parking Maneuver: <a href="matlab:sm_car_test_parking">Run</a>, <a href="matlab:edit('sm_car_test_parking.m');">Edit Code</a>, <a href="matlab:web('sm_car_test_parking.png');">Results</a><br>
 % <tr>7.  Driving in Reverse: <a href="matlab:sm_car_test_dlc_reverse">Run</a>, <a href="matlab:edit('sm_car_test_dlc_reverse.m');">Edit Code</a>, <a href="matlab:web('sm_car_test_dlc_reverse.png');">Results</a><br>
-% <tr>8.  Generate GGV Diagram: <a href="matlab:sm_car_ggv_diagram_examples">Run</a>, <a href="matlab:edit('sm_car_ggv_diagram_examples.m');">Edit Code</a>, <a href="matlab:web('sm_car_ggv_diagram_examples.html');">Results</a><br>
-% <tr>9.  Display Battery Status: <a href="matlab:sm_car_display_battery_test">Run</a>, <a href="matlab:edit('sm_car_display_battery_test.m');">Edit Code</a><br>
-% <tr>10. Protected Mode: <a href="matlab:edit('sm_car_test_protected_mode.m');">Edit Code</a><br>
-% <tr>11. Powertrain Configurations: <a href="matlab:web('sm_car_testing_powertrain_configs.html');">Results</a>, <a href="matlab:edit('sm_car_testing_powertrain_configs');">Edit Code</a><br>
+% <tr>8.  Hydropneumatic Suspension: <a href="matlab:sm_car_test_hydropneu_susp">Run</a>, <a href="matlab:edit('sm_car_test_hydropneu_susp.m');">Edit Code</a>, <a href="matlab:web('sm_car_test_hydropneu_susp.png');">Results</a><br>
+% <tr>9.  Generate GGV Diagram: <a href="matlab:sm_car_ggv_diagram_examples">Run</a>, <a href="matlab:edit('sm_car_ggv_diagram_examples.m');">Edit Code</a>, <a href="matlab:web('sm_car_ggv_diagram_examples.html');">Results</a><br>
+% <tr>10. Lookup-Table Suspension: <a href="matlab:sm_car_test_gen_data_lut_susp">Run</a>, <a href="matlab:edit('sm_car_test_gen_data_lut_susp.m');">Edit Code</a>, <a href="matlab:web('sm_car_test_gen_data_lut_susp.png');">Results</a><br>
+% <tr>11. Display Battery Status: <a href="matlab:sm_car_display_battery_test">Run</a>, <a href="matlab:edit('sm_car_display_battery_test.m');">Edit Code</a><br>
+% <tr>12. Protected Mode: <a href="matlab:edit('sm_car_test_protected_mode.m');">Edit Code</a><br>
+% <tr>13. Powertrain Configurations: <a href="matlab:web('sm_car_testing_powertrain_configs.html');">Results</a>, <a href="matlab:edit('sm_car_testing_powertrain_configs');">Edit Code</a><br>
 % <br>
 % <tr><b><u>Testrigs</u></b><br>
 % <tr>1.  Quarter-Car Testrig: <a href="matlab:cd(fileparts(which('testrig_quarter_car.slx')));open_system('testrig_quarter_car');">Open Model</a>, <a href="matlab:web('testrig_quarter_car_results.html');"> Documentation</a><br>
---
+++ Workflows/Testing/sm_car_test_hydropneu_susp.m
@@ -0,0 +1,64 @@
+% Script to test hydropneumatic suspension in SUV 
+% Manuever is low speed steering.
+
+% Test four wheel steering
+modelName = 'sm_car';
+open_system(modelName);
+
+% Load Vehicle data structure 
+sm_car_load_vehicle_data(modelName,'241');
+Vehicle.Powertrain.Driveline = VDatabase.Driveline.Axle2_L1_R1_L2_R2_default;
+Vehicle.Powertrain.Power     = VDatabase.Power.Ideal_L1_R1_L2_R2_default;
+
+% Disable rear steering
+Control.Default.Steer.Gain.nRearAct.Value = 0;
+% Enable bushings at connection lower control arm to axle
+Vehicle.Chassis.SuspA1.AxleTA2PR.Arm_to_Axle.class.Value = 'Kinematic';
+Vehicle.Chassis.SuspA2.AxleTA3.Arm_to_Axle.class.Value = 'Kinematic';
+
+% Make chassis transparent
+Vehicle.Chassis.Body.BodyGeometry.Opacity.Value = 0.3;
+
+% Select Maneuver
+sm_car_config_maneuver('sm_car','Low Speed Steer');
+
+% Enable controller for hydropneumatic suspension
+set_param([modelName '/Controller/Default'],'popup_spring_control_A1','Hydropneumatic');
+set_param([modelName '/Controller/Default'],'popup_spring_control_A2','Hydropneumatic');
+
+% Set gas spring to soft
+set_param([modelName '/Controller/Default'],'popup_gas_spring_A1','Soft');
+set_param([modelName '/Controller/Default'],'popup_gas_spring_A2','Soft');
+
+% Run test
+sim(modelName)
+
+% Save vehicle roll angle
+sim_soft_t     = sm_car_sim_res_get(logsout_sm_car,simlog_sm_car,Vehicle,'time');
+sim_soft_aRoll = sm_car_sim_res_get(logsout_sm_car,simlog_sm_car,Vehicle,'aRoll');
+
+% Set gas spring to soft
+set_param([modelName '/Controller/Default'],'popup_gas_spring_A1','Stiff');
+set_param([modelName '/Controller/Default'],'popup_gas_spring_A2','Stiff');
+
+% Run test
+sim(modelName)
+
+% Save vehicle roll angle
+sim_stiff_t     = sm_car_sim_res_get(logsout_sm_car,simlog_sm_car,Vehicle,'time');
+sim_stiff_aRoll = sm_car_sim_res_get(logsout_sm_car,simlog_sm_car,Vehicle,'aRoll');
+
+figure(109)
+plot(sim_soft_t.data,sim_soft_aRoll.data,'LineWidth',1,'DisplayName','Soft')
+hold on
+plot(sim_stiff_t.data,sim_stiff_aRoll.data,'LineWidth',1,'DisplayName','Stiff')
+hold off
+legend('Location','Best');
+xlabel('Time (s)')
+ylabel(['Vehicle Roll (' sim_stiff_aRoll.units ')']);
+grid on
+
+title('Comparison Hydropneumatic Spring Setting')
+
+% If required, save image
+%saveas(gcf,[mfilename '.png'])
